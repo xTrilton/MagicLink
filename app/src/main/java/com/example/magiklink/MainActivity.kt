9b6e50eaaf67d3b71522634b85265f295b214788
Boemo Wame Mmopelwa <boemommopelwa@gmail.com>
2022-01-19 10:24:04 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/magiklink/MainActivity.kt
+++ app/src/main/java/com/example/magiklink/MainActivity.kt
@@ -10,17 +10,26 @@ import link.magic.android.modules.auth.response.DIDToken
 
 class MainActivity : AppCompatActivity() {
     lateinit var magic: Magic
+     private lateinit var emailEditText: EditText
+    private lateinit var smsEditText: EditText
+    var email = String()
+    var sms = String()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        emailEditText = findViewById(R.id.costEditText)
+        smsEditText = findViewById(R.id.costEditText)
+        
         magic = (applicationContext as MagiclinkInstance).magic
         magic.startRelayer(this)
     }
 
     fun emailAuthentication(view: View) {
+          email = emailEditText.text.toString()
 
-        val result = magic.auth.loginWithMagicLink("hello@example.com")
+        val result = magic.auth.loginWithMagicLink(email)
 
         result.whenComplete { response: DIDToken?, error: Throwable? ->
             if (error != null) {
@@ -28,13 +37,16 @@ class MainActivity : AppCompatActivity() {
             }
             if (response != null && !response.hasError()) {
                 Log.d("Magic", "You're logged in." + response.result)
+                 intent = Intent(applicationContext, HomeActivity::class.java)  
+                startActivity(intent) 
             } else {
                 Log.d("login", "Not Logged in")
             }
         }
     }
     fun smsAuthtentication(view: View) {
-
+        sms = emailEditText.text.toString()
+        //please introspect the line below
         val configuration = LoginWithSMSConfiguration(phoneNumber.text.toString())
         val result = magic.auth.loginWithSMS(configuration)
 
@@ -44,6 +56,8 @@ class MainActivity : AppCompatActivity() {
             }
             if (response != null && !response.hasError()) {
                 Log.d("Magic", "You're logged in." + response.result)
+                intent = Intent(applicationContext, HomeActivity::class.java)  
+                startActivity(intent)  
             } else {
                 Log.d("login", "Not Logged in")
             }
